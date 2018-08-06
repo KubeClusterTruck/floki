baa8b79520a6ca863fe5e9d64a3bc1ce859102d3
Greg Hoelzer <grhoelze@microsoft.com>
2018-08-06 13:43:51 -0500

    removed nodejs project directory

--- mvp01-nodejs/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=floki-mvp01-Web-Deploy.pubxml -p:Password=7lc5TXcKvfLq1llT3pW24apwTj3v964l1pnitB32Zay6cjxfizokz75J1pQe
-
