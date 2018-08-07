728fc62a11b4510a6cc98d65022bdf4fdaae58ef
Greg Hoelzer <greg.hoelzer@microsoft.com>
2018-08-07 11:09:09 -0500

    completed local git setup

--- mvp01-floki-bot/app.js
+++ mvp01-floki-bot/app.js
@@ -29,7 +29,7 @@ server.post('/api/messages', connector.listen());
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
 
-var tableName = 'floki-botdata';
+var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
